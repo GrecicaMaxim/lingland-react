9a18de99a4d13e082c8887c86c09ad74ad7d0e5e
GrecicaMaxim <greckamaksim872@gmail.com>
2026-05-03 21:58:05 +0300

    Created JSON file and mapped language options

--- src/components/lang-choice/index.jsx
+++ src/components/lang-choice/index.jsx
@@ -1,25 +1,19 @@
 import LanguageOption from "../language-option";
+import mapping_data from "@/mapping_data.json";
+
+const language_option = mapping_data.lang_option;
 
 export default function LanguageChoice() {
     return (
         <div className="lang-choice">
             <h2>What language do you want to learn?</h2>
             <div className="lang-list">
-                <LanguageOption source="/flag-uk.png" name="English" />
-                <LanguageOption source="/flag-germany.png" name="German" />
-                <LanguageOption source="/flag-france.png" name="French" />
-                <LanguageOption source="/flag-spain.png" name="Spanish" />
-                <LanguageOption source="/flag-russia.png" name="Russian" />
-                <LanguageOption source="/flag-japan.png" name="Japanese" />
-                <LanguageOption source="/flag-south-korea.png" name="Korean" />
-                <LanguageOption source="/flag-italy.png" name="Italian" />
-                <LanguageOption source="/flag-portugal.png" name="Portuguese" />
-                <LanguageOption source="/flag-china.png" name="Chinese" />
-                <LanguageOption source="/flag-poland.png" name="Polish" />
-                <LanguageOption source="/flag-netherlands.png" name="Dutch" />
+                {language_option.map((n) => (
+                    <LanguageOption source={n.Source} name={n.Name}/>
+                ))}
                 <div></div>
-                <LanguageOption source="/flag-saudi-arabia.png" name="Arabic" />
-                <LanguageOption source="/flag-turkey.png" name="Turkish" />
+                <LanguageOption source="/flag-saudi-arabia.png" name="Arabic"/>
+                <LanguageOption source="/flag-turkey.png" name="Turkish"/>
                 <div></div>
             </div>
         </div>
